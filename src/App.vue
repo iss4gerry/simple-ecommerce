0aa7100218a8da18c54adb8c8ed4606183342a59
Gerry <gerrydesrian488@gmail.com>
2024-08-23 15:10:56 +0700

    price slider component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>Mayu Shop</h1>
-    <product-list :products="products" :maximum="maximum"></product-list>
+    <price-slider :sliderStatus="sliderStatus" v-model:maximum="maximum"></price-slider>
+    <product-list :products="products" :maximum="maximum" @add="addItem"></product-list>
   </div>
 </template>
 
 <script>
 // import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import ProductList from './components/ProductList.vue';
+import PriceSlider from './components/PriceSlider.vue';
 
 export default {
   name: 'App',
@@ -15,11 +17,32 @@ export default {
     return {
       maximum: 50,
       products: [],
+      cart: [],
+      sliderStatus:true
     }
   },
   components: {
     // FontAwesomeIcon,
+    PriceSlider,
     ProductList
+  },methods: {
+    addItem: function(product) {
+            var productIndex
+            var productExist = this.cart.filter(function (item, index) {
+                if(item.product.id == Number(product.id)){
+                    productIndex = index
+                    return true
+                }else{
+                    return false 
+                }
+            })
+
+            if(productExist.length){
+                this.cart[productIndex].qty++
+            }else{
+                this.cart.push({product: product, qty: 1})
+            }
+        }
   },
   mounted: function() {
         fetch('https://hplussport.com/api/products/order/price')
