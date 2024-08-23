3084b4bedacc3b3bd68dfaf9caae1e112ee83c7a
Gerry <gerrydesrian488@gmail.com>
2024-08-23 13:49:58 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mt-5">
+    <h1>Mayu Shop</h1>
+    <p class="animate__animated animate__fadeInRight">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem impedit voluptates, ex culpa, mollitia laudantium et dignissimos ut eos recusandae excepturi beatae consectetur voluptatibus. Et odit dolorem necessitatibus doloribus nulla?
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FontAwesomeIcon
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
